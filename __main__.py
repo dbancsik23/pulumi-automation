f14ca9b28ec358d273d6e15e796d63275ef2571c
Bancsik David <dbancsik@gmail.com>
2024-12-31 00:56:05 +0200

    Add Docker support and configure containerized services
    Introduce Docker and Docker Compose to containerize the application. Added a `Dockerfile` and `docker-compose.yml` for development and deployment, including a PostgreSQL service. Also updated dependencies and adjusted the server host for container compatibility.

--- __main__.py
+++ __main__.py
@@ -4,4 +4,4 @@ from app.db.db import auto_create_db
 
 if __name__ == "__main__":
     auto_create_db()
-    uvicorn.run("app.main:app", host="127.0.0.1", port=8001, reload=True)
+    uvicorn.run("app.main:app", host="0.0.0.0", port=8001, reload=True)
